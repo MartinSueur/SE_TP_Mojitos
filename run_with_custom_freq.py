eb5b27cd6c0b0b0ad3c9b863f7bb2809109128f8
MartinSueur <martin@zforce.eu>
2025-11-12 11:32:57 +0100

    New files

---
+++ run_with_custom_freq.py
@@ -0,0 +1,6 @@
+import sys
+import os
+
+#min_freq = open("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq", "r").readline()
+
+print("0" < "1")
---
+++ sorting.py
@@ -0,0 +1,14 @@
+from sorting_techniques import pysort
+
+sortObj = pysort.Sorting()
+
+myList = [1,3,2,7,4,89,56,99,111,23,6445,566]
+
+# Genearting the Sort Result 
+sortResult = sortObj.bubbleSort(myList)
+
+# The SortResult contains the Sorted List
+
+print(sortResult)
+
+print(str(x)+str(y))
