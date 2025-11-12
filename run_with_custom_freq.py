b1086d797136104b274c3f416b08bea1c8d1740e
MartinSueur <martin@zforce.eu>
2025-11-12 11:37:58 +0100

    testing reading files

--- run_with_custom_freq.py
+++ run_with_custom_freq.py
@@ -1,6 +1,6 @@
 import sys
 import os
 
-#min_freq = open("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq", "r").readline()
-
-print("0" > "1")
+min_freq = open("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq", "r").readline()
+max_freq = open("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq", "r").readline()
+print("freq between : ",min_freq,max_freq)
